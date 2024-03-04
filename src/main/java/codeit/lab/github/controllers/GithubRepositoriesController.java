c3ffa13c5780a06deb4b358804d8e797e92b367b
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-04 14:27:38 +0100

    Add: added basic controller

---
+++ src/main/java/codeit/lab/github/controllers/GithubRepositoriesController.java
@@ -0,0 +1,11 @@
+package codeit.lab.github.controllers;
+
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GithubRepositoriesController {
+
+
+}
